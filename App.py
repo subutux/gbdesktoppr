c4d98c6e61cdb095ea0a0bc2d5511ba0680492c8
stijn <stijn@cmelectronics.be>
2014-06-24 13:47:22 +0200

    Default username is now empty & asks for username if empty

--- App.py
+++ App.py
@@ -154,11 +154,16 @@ class guiApp(object):
 		self.window.set_title ("Gnome Desktoppr")
 		self.window.connect('delete-event',Gtk.main_quit)
 		self.api = desktoppr.api()
-		builder.get_object("username_input").set_text(appSettings.get_string("username"))
-		self.t = DownloadWallpaperThumbs(api,store,appSettings.get_string("username"),localTempStorage)
-		self.t.start()
+		if appSettings.get_string("username") != "":
+			builder.get_object("username_input").set_text(appSettings.get_string("username"))
+			self.t = DownloadWallpaperThumbs(api,store,appSettings.get_string("username"),localTempStorage)
+			self.t.start()
 	def run(self):
 		self.window.show_all()
+		# If no username is defined, show the settings
+		if appSettings.get_string("username") == "":
+			builder.get_object('login_window').show_all()
+
 		Gtk.main()
 
 if args.randomize:
